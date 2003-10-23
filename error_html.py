c44b264e5449733bc2149399b73fc11c1179f10e
John Holland <azoner@gmail.com>
2003-10-23 18:40:33 +0000

    initial

---
+++ error_html.py
@@ -0,0 +1,109 @@
+#    $Id$
+#    This file is part of the pyX12 project.
+#
+#    Copyright (c) 2001-2004 Kalamazoo Community Mental Health Services,
+#                John Holland <jholland@kazoocmh.org> <john@zoner.org>
+#
+#    All rights reserved.
+#
+#        Redistribution and use in source and binary forms, with or without modification, 
+#        are permitted provided that the following conditions are met:
+#
+#        1. Redistributions of source code must retain the above copyright notice, this list 
+#           of conditions and the following disclaimer. 
+#        
+#        2. Redistributions in binary form must reproduce the above copyright notice, this 
+#           list of conditions and the following disclaimer in the documentation and/or other 
+#           materials provided with the distribution. 
+#        
+#        3. The name of the author may not be used to endorse or promote products derived 
+#           from this software without specific prior written permission. 
+#
+#        THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED 
+#        WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+#        MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO 
+#        EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+#        EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
+#        OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+#        INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+#        CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+#        ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+#        THE POSSIBILITY OF SUCH DAMAGE.
+
+
+"""
+Generates HTML error output
+"""
+
+#import os
+#import sys
+import string
+from types import *
+import time
+import logging
+
+# Intrapackage imports
+from errors import *
+from x12file import seg_str
+
+__author__  = "John Holland <jholland@kazoocmh.org> <john@zoner.org>"
+
+logger = logging.getLogger('pyx12.error_html')
+logger.setLevel(logging.DEBUG)
+#logger.setLevel(logging.ERROR)
+
+class error_html:
+    """
+    Class:      error_html
+    Desc:    
+    """
+    def __init__(self, errh, fd, term=('~', '*', '~', '\n')): 
+        """
+        Class:      error_html
+        Name:       __init__
+        Desc:    
+        Params:     fd - target file
+                    term - tuple of x12 terminators used
+        """
+        self.errh = errh
+        self.fd = fd
+        self.seg_term = term[0]
+        self.ele_term = term[1]
+        self.subele_term = term[2]
+        self.eol = ''
+
+    def header(self):
+        self.fd.write('<html>\n<head>\n')
+        self.fd.write('<title>pyx12 Error Analysis</title>\n')
+        self.fd.write('<style type="text/css">\n<!--\n')
+        self.fd.write('  span.seg { color: black; font-style: normal; }\n')
+        self.fd.write('  span.error { background-color: #CCCCFF; color: red; font-style: normal; }\n')
+        self.fd.write('-->\n</style>\n')
+        self.fd.write('</head>\n<body>\n')
+        self.fd.write('<h1>pyx12 Error Analysis</h1>\n<h3>Analysis Date: %s</h3><p>\n' % \
+            (time.strftime('%y%m%d %H%M%S')))
+        self.fd.write('<div class="segs" style="">\n')
+
+    def footer(self):
+        self.fd.write('</div>\n')
+        self.fd.write('<p>\n<a href="http://sourceforge.net/projects/pyx12/">pyx12 project page</a>\n</p>\n')
+        self.fd.write('</body>\n</html>\n')
+
+    def print_seg(self, seg):
+        """
+        Class:      error_html 
+        Name:       visit_root_pre
+        Desc:    
+        Params:     seg - list of elements
+        """
+        self.fd.write('<span class="seg">%s</span><br>\n' % (self._seg_str(seg)))
+        
+    def _seg_str(self, seg):
+        """
+        Class:      error_html
+        Name:       _seg_str
+        Desc:    
+        Params:     seg - list of elements
+        """
+        return seg_str(seg, self.seg_term, self.ele_term, self.subele_term, \
+            self.eol).replace(' ', '&nbsp;')
