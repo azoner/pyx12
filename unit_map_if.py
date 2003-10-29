4a8fa0ad300ca8fbf1a0883bb6446442d6777968
John Holland <azoner@gmail.com>
2003-10-29 20:15:02 +0000

    syntax

--- unit_map_if.py
+++ unit_map_if.py
@@ -1,41 +1,25 @@
 #! /usr/bin/env /usr/local/bin/python
 
-#import test_support
-#from test_support import TestFailed, have_unicode
 import unittest
-#import sys
-import StringIO
 import pdb
 
 import error_handler
-#from error_handler import ErrorErrhNull
 from errors import *
-import x12file
+import map_if
 
-class ISA_header(unittest.TestCase):
+class IsValidSyntaxP(unittest.TestCase):
 
     def setUp(self):
-        self.fd = None
         self.errh = error_handler.errh_null()
 
-    def test_starts_with_ISA(self):
-        self.fd = StringIO.StringIO(' ISA~')
-        #src = x12file.x12file(self.fd, self.errh)
-        #self.assertEqual(self.errh.err_cde, 'ISA1', self.errh.err_str)
-        self.failUnlessRaises(x12file.x12Error, x12file.x12file, self.fd, self.errh)
-
-    def test_at_least_ISA_len(self):
-        self.fd = StringIO.StringIO('ISA~')
-        #src = x12file.x12file(self.fd, self.errh)
-        #self.assertEqual(self.errh.err_cde, 'ISA4', self.errh.err_str)
-        self.failUnlessRaises(x12file.x12Error, x12file.x12file, self.fd, self.errh)
-
-    def tearDown(self):
-        self.fd.close()
-
+    def test_P_ok(self):
+        seg = ['NM1', '41']
+        syntax = ['P', 5, 4]
+        map_if._is_syntax_valid(seg, self.errh, syntax)
+        self.assertEqual(self.errh.err_cde, '2', self.errh.err_str)
 
 def suite():
-    suite = unittest.makeSuite((ISA_header, IEA_Checks, GE_Checks, SE_Checks))
+    suite = unittest.makeSuite((IsValidSyntaxChecks)
     return suite
                 
 if __name__ == "__main__":
