62b286c86cc7f98c7ab52e4e4794e8f6c4fdc976
John Holland <azoner@gmail.com>
2003-10-31 05:21:37 +0000

    unit testing

--- unit_map_walker.py
+++ unit_map_walker.py
@@ -14,36 +14,7 @@ import map_if
 
 class Explicit_Loops(unittest.TestCase):
     """
-    SE to ST - repeat ST loop
-    SE to GE - end ST loop
-    GE to GS - repeat GS loop
-    GE to IEA - end GS loop
-    IEA to ISA - repeat ISA
-
     FAIL - no end tag for explicit loop
-    FAIL - Mandatory segment skipped
-    FAIL - Mandatory loop skipped
-
-    TA1 segment
-
-    Repeat of segment
-    repeat of loop
-    child loop
-    next sibling loop
-    end loop - goto parent
-
-    start at loop node
-    start at segment node
-    start at element/composite node?
-
-    MATCH regular segment
-    MATCH loop by first segment
-    MATCH ID segment
-    MATCH HL segment
-
-    FAIL - found not used segment
-    FAIL - segment repeat exceeds max count
-
     """
     def setUp(self):
         self.walker = walk_tree()
@@ -63,9 +34,33 @@ class Explicit_Loops(unittest.TestCase):
         node = self.walker.walk(node, seg, self.errh, 5, 4, None)
         self.assertEqual(seg[0], node.id)
 
-    def test_ST_to_BHT(self):
-        node = self.map.getnodebypath('/ST')
-        seg = ['BHT', '0019']
+    def test_SE_to_ST(self):
+        node = self.map.getnodebypath('/SE')
+        seg = ['ST', '837']
+        node = self.walker.walk(node, seg, self.errh, 5, 4, None)
+        self.assertEqual(seg[0], node.id)
+
+    def test_SE_to_GE(self):
+        node = self.map.getnodebypath('/SE')
+        seg = ['GE', '1']
+        node = self.walker.walk(node, seg, self.errh, 5, 4, None)
+        self.assertEqual(seg[0], node.id)
+
+    def test_GE_to_GS(self):
+        node = self.map.getnodebypath('/GE')
+        seg = ['GS', '1']
+        node = self.walker.walk(node, seg, self.errh, 5, 4, None)
+        self.assertEqual(seg[0], node.id)
+
+    def test_GE_to_IEA(self):
+        node = self.map.getnodebypath('/GE')
+        seg = ['IEA', '1']
+        node = self.walker.walk(node, seg, self.errh, 5, 4, None)
+        self.assertEqual(seg[0], node.id)
+
+    def test_IEA_to_ISA(self):
+        node = self.map.getnodebypath('/IEA')
+        seg = ['ISA', '1']
         node = self.walker.walk(node, seg, self.errh, 5, 4, None)
         self.assertEqual(seg[0], node.id)
 
@@ -75,7 +70,79 @@ class Explicit_Loops(unittest.TestCase):
 #        node = self.walker.walk(node, seg, self.errh, 5, 4, None)
 #        self.assertNotEqual(seg[0], node.id)
 
+class Implicit_Loops(unittest.TestCase):
+    """
+    FAIL - Mandatory segment skipped
+    FAIL - Mandatory loop skipped
 
+    TA1 segment
+
+    child loop
+    next sibling loop
+    end loop - goto parent loop
+    
+    start at loop node
+    start at segment node
+    start at element/composite node?
+
+    MATCH loop by first segment
+    MATCH HL segment
+
+    FAIL - loop repeat exceeds max count
+    OK - loop repeat does not exceed max count
+    
+
+    """
+
+    def setUp(self):
+        self.walker = walk_tree()
+        #self.map = map_if.map_if('map/837.4010.X098.A1.xml')
+        self.map = map_if.load_map_file('map/837.4010.X098.A1.xml')
+        self.errh = error_handler.errh_null()
+
+    def test_ST_to_BHT(self):
+        node = self.map.getnodebypath('/ST')
+        seg = ['BHT', '0019']
+        node = self.walker.walk(node, seg, self.errh, 5, 4, None)
+        self.assertEqual(seg[0], node.id)
+
+class SegmentWalk(unittest.TestCase):
+    """
+    FAIL - segment repeat exceeds max count
+    OK - segment repeat does not exceed max count
+    FAIL - found not used segment
+    FAIL - segment was not found
+    """
+
+    def setUp(self):
+        self.walker = walk_tree()
+        self.map = map_if.load_map_file('map/837.4010.X098.A1.xml')
+        self.errh = error_handler.errh_null()
+
+    def test_match_regular_segment(self):
+        node = self.map.getnodebypath('/2000A/2010AB/NM1')
+        seg = ['N4', 'Billings', 'MT', '56123']
+        node = self.walker.walk(node, seg, self.errh, 5, 4, None)
+        self.assertEqual(seg[0], node.id)
+    
+    def test_match_ID_segment1(self):
+        node = self.map.getnodebypath('/2000A/2000B/2300/CLM')
+        seg = ['DTP', '454', 'D8', '20040101']
+        node = self.walker.walk(node, seg, self.errh, 5, 4, None)
+        self.assertEqual(seg[0], node.id)
+    
+    def test_match_ID_segment2(self):
+        node = self.map.getnodebypath('/2000A/2000B/2300/CLM')
+        seg = ['DTP', '304', 'D8', '20040201']
+        node = self.walker.walk(node, seg, self.errh, 5, 4, None)
+        self.assertEqual(seg[0], node.id)
+    
+    def test_fail_ID_segment(self):
+        node = self.map.getnodebypath('/2000A/2000B/2300/CLM')
+        seg = ['DTP', '999', 'D8', '20040201']
+        node = self.walker.walk(node, seg, self.errh, 5, 4, None)
+        self.assertNotEqual(seg[0], node.id)
+    
 def suite():
     suite = unittest.makeSuite((Explicit_Loops))
     return suite
