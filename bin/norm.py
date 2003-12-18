b4a62ff9b4bd2f6beb6b3e9a53d84ae929a6e611
John Holland <azoner@gmail.com>
2003-12-18 21:33:35 +0000

    paths

--- bin/norm.py
+++ bin/norm.py
@@ -1,11 +1,11 @@
 #! /usr/bin/env /usr/local/bin/python
 
-import x12file
-import error_handler
+import pyx12.x12file
+import pyx12.error_handler
 import sys
 
-errh = error_handler.err_handler()
+errh = pyx12.error_handler.err_handler()
 fd = sys.stdin
-src = x12file.x12file(fd, errh)
+src = pyx12.x12file.x12file(fd, errh)
 for c in src:
     src.print_seg(c)
