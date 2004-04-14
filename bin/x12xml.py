3b90c764214a91810ec548ec298355768dcc14af
John Holland <azoner@gmail.com>
2004-04-14 18:31:08 +0000

    command line params

--- bin/x12xml.py
+++ bin/x12xml.py
@@ -39,30 +39,17 @@ Validate against a map and codeset values.
 Create a XML document based on the data file
 """
 
-#import time
 import os, os.path
-#import stat
 import sys
 import logging
 #import string
 from types import *
 #import StringIO
 #import tempfile
-import pdb
+#import pdb
 #import profile
 
 # Intrapackage imports
-#import error_handler
-#import error_997
-#import error_debug
-#import error_html
-#from errors import *
-#import codes
-#import map_index
-#import map_if
-#import x12file
-#from map_walker import walk_tree
-#from utils import *
 #import pyx12.x12xml_simple
 #import pyx12.x12xml_idtag
 import pyx12
@@ -76,14 +63,25 @@ __version__ = pyx12.__version__
 __date__    = pyx12.__date__
 
 def usage():
-    sys.stdout.write('usage: x12xml source_files\n')
+    sys.stdout.write('x12xml.py %s (%s)\n' % (__version__, __date__))
+    sys.stdout.write('usage: x12lint.py [options] source_files\n')
+    sys.stdout.write('\noptions:\n')
+    sys.stdout.write('  -c <b|e>   Specify X12 character set: b=basic, e=extended\n')
+    sys.stdout.write('  -f         Force map load.  Do not use the map pickle file\n')
+    sys.stdout.write('  -H         Create HTML output file\n')
+    sys.stdout.write('  -l <file>  Output log\n')
+    sys.stdout.write('  -m <path>  Path to map files\n')
+    sys.stdout.write('  -p <path>  Path to to pickle files\n')
+    sys.stdout.write('  -q         Quiet output\n')
+    sys.stdout.write('  -v         Verbose output\n')
+    sys.stdout.write('  -x <tag>   Exclude external code\n')
 
 def main():
     """Script main program."""
     import getopt
     param = pyx12.params.params()
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'qvc:')
+        opts, args = getopt.getopt(sys.argv[1:], 'c:fl:m:p:qvx:H')
     except getopt.error, msg:
         usage()
         raise
@@ -103,6 +101,18 @@ def main():
         if o == '-v': logger.setLevel(logging.DEBUG)
         if o == '-q': logger.setLevel(logging.ERROR)
         if o == '-c': param.set_param('charset', a)
+        if o == '-x': param.set_param('exclude_external_codes', a)
+        if o == '-f': param.set_param('force_map_load', True)
+        if o == '-m': param.set_param('map_path', a)
+        if o == '-p': param.set_param('pickle_path', a)
+        if o == '-H': flag_html = True
+        if o == '-l':
+            try:
+                hdlr = logging.FileHandler(a)
+                hdlr.setFormatter(formatter)
+                logger.addHandler(hdlr)
+            except IOError:
+                logger.error('Could not open log file: %s' % (a))
 
     for src_filename in args:
         target_xml = os.path.splitext(src_filename)[0] + '.xml'
@@ -116,7 +126,6 @@ def main():
 
         try:
             result = pyx12.x12n_document.x12n_document(param, src_filename, None, None, fd_xml)
-            fd_src.close()
             fd_xml.close()
             if not result:
                 logger.error('File %s had errors.  XML file was not created.' \
