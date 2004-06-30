4fd1ae08c95f05a10ca95bf6ca2a2e7637fc67df
John Holland <azoner@gmail.com>
2004-06-30 21:12:28 +0000

    turn off psyco when debugging

--- bin/x12lint.py
+++ bin/x12lint.py
@@ -61,6 +61,7 @@ def usage():
     sys.stdout.write('usage: x12lint.py [options] source_files\n')
     sys.stdout.write('\noptions:\n')
     sys.stdout.write('  -c <b|e>   Specify X12 character set: b=basic, e=extended\n')
+    sys.stdout.write('  -d         Debug Mode.  Implies verbose output\n')
     sys.stdout.write('  -f         Force map load.  Do not use the map pickle file\n')
     sys.stdout.write('  -H         Create HTML output file\n')
     sys.stdout.write('  -l <file>  Output log\n')
@@ -78,7 +79,7 @@ def main():
     import getopt
     param = pyx12.params.params()
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'c:fl:m:p:qvx:HP')
+        opts, args = getopt.getopt(sys.argv[1:], 'c:dfl:m:p:qvx:HP')
     except getopt.error, msg:
         usage()
         sys.exit(2)
@@ -97,12 +98,16 @@ def main():
     flag_html = False
     flag_997 = True
     profile = False
+    debug = False
     #param.set_param('map_path', os.path.expanduser('/usr/local/share/pyx12/map'))
     #param.set_param('pickle_path', os.path.expanduser('/tmp'))
     for o, a in opts:
         if o == '-v': logger.setLevel(logging.DEBUG)
         if o == '-q': logger.setLevel(logging.ERROR)
         if o == '-c': param.set_param('charset', a)
+        if o == '-d': 
+            param.set_param('debug', True)
+            debug = True
         if o == '-x': param.set_param('exclude_external_codes', a)
         if o == '-f': param.set_param('force_map_load', True)
         if o == '-m': param.set_param('map_path', a)
@@ -117,6 +122,13 @@ def main():
             except IOError:
                 logger.error('Could not open log file: %s' % (a))
         #if o == '-9': target_997 = os.path.splitext(src_filename)[0] + '.997'
+
+    if debug:
+        try:
+            import psyco
+            psyco.full()
+        except ImportError:
+            pass
 
     for src_filename in args:
         try:
@@ -159,11 +171,5 @@ if __name__ == '__main__':
     #    import profile
     #    profile.run('pyx12.x12n_document(src_filename)', 'pyx12.prof')
     #else:
-    try:
-        import psyco
-        psyco.full()
-    except ImportError:
-        pass
-
     sys.exit(not main())
 
