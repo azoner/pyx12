23899eff2dd19dd0c3304b6aad98f57ce8ccc7e8
John Holland <azoner@gmail.com>
2004-11-22 21:48:10 +0000

    add test C++ files

---
+++ pyx12/test1.cxx
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+
+#include "segment.hxx"
+
+using namespace std;
+
+// most frequently you implement test cases as a free functions
+int main()
+{
+    std::string seg_str("TST*AA*1*Y*BB:5*ZZ");
+    cout << seg_str << endl;
+    Pyx12::segment seg_data(seg_str, "~", "*", ":");
+//    cout << seg_data << endl;
+}
