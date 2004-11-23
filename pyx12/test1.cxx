a6ee156f25462296317dde84ed68dbce3a515768
John Holland <azoner@gmail.com>
2004-11-23 22:52:11 +0000

    fix test

--- pyx12/test1.cxx
+++ pyx12/test1.cxx
@@ -8,9 +8,9 @@ using namespace std;
 // most frequently you implement test cases as a free functions
 int main()
 {
-    std::string seg_str("TST*AA*1*Y*BB:5*ZZ");
-    cout << seg_str << endl;
+    std::string seg_str("TST*AA*1*Y*BB:5:4:AA*ZZ~");
+    //cout << seg_str << endl;
     Pyx12::segment seg_data(seg_str, "~", "*", ":");
-//    cout << seg_data << endl;
+    cout << seg_data << endl;
     return 1;
 }
