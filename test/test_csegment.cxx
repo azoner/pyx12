90212a4c254ac0a2cb5c31158bb93ccfa061c244
John Holland <azoner@gmail.com>
2004-11-24 06:34:07 +0000

    add C++ segment unit tests

--- test/test_csegment.cxx
+++ test/test_csegment.cxx
@@ -28,6 +28,56 @@ void test_identity3()
     BOOST_CHECK(seg_data.format() == seg_str); 
 }
 
+// Test arbitrary terminators
+void ArbitraryIdentity()
+{
+    std::string seg_str("TST&AA!1!1&BB!5&ZZ");
+    Pyx12::segment seg_data(seg_str, "+", "&", "!");
+    BOOST_CHECK(seg_data.format() == seg_str + '+'); 
+}
+
+void ArbitraryGetSegID()
+{
+    std::string seg_str("TST&AA!1!1&BB!5&ZZ");
+    Pyx12::segment seg_data(seg_str, "+", "&", "!");
+    BOOST_CHECK(seg_data.get_seg_id() == "TST"); 
+}
+
+void ArbitraryLength()
+{
+    std::string seg_str("TST&AA!1!1&BB!5&ZZ");
+    Pyx12::segment seg_data(seg_str, "+", "&", "!");
+    BOOST_CHECK(seg_data.length() == 3); 
+}
+
+void ArbitraryGetItem3()
+{
+    std::string seg_str("TST&AA!1!1&BB!5&ZZ");
+    Pyx12::segment seg_data(seg_str, "+", "&", "!");
+    BOOST_CHECK(seg_data[2][0].get_value() == "ZZ"); 
+}
+
+void ArbitraryGetItem1()
+{
+    std::string seg_str("TST&AA!1!1&BB!5&ZZ");
+    Pyx12::segment seg_data(seg_str, "+", "&", "!");
+    BOOST_CHECK(seg_data[0].format() == "AA!1!1"); 
+}
+/*
+void ArbitraryGetItemMinus1()
+{
+    std::string seg_str("TST&AA!1!1&BB!5&ZZ");
+    Pyx12::segment seg_data(seg_str, "+", "&", "!");
+    BOOST_CHECK(seg_data[-1][0].get_value() == "ZZ"); 
+}
+
+void ArbitraryOtherTerms()
+{
+    std::string seg_str("TST&AA!1!1&BB!5&ZZ");
+    Pyx12::segment seg_data(seg_str, "+", "&", "!");
+    BOOST_CHECK(seg_data.format('~', '*', ':') == "TST*AA:1:1*BB:5*ZZ~");
+}
+*/
 
 test_suite*
 init_unit_test_suite( int, char* [] ) {
@@ -37,6 +87,13 @@ init_unit_test_suite( int, char* [] ) {
     test->add( BOOST_TEST_CASE( &test_identity1), 0);
     test->add( BOOST_TEST_CASE( &test_identity2), 0);
     test->add( BOOST_TEST_CASE( &test_identity3), 0);
+    test->add( BOOST_TEST_CASE( &ArbitraryIdentity), 0);
+    test->add( BOOST_TEST_CASE( &ArbitraryGetSegID), 0);
+    test->add( BOOST_TEST_CASE( &ArbitraryLength), 0);
+    test->add( BOOST_TEST_CASE( &ArbitraryGetItem3), 0);
+    test->add( BOOST_TEST_CASE( &ArbitraryGetItem1), 0);
+    //test->add( BOOST_TEST_CASE( &ArbitraryGetItemMinus1), 0);
+    //test->add( BOOST_TEST_CASE( &ArbitraryOtherTerms), 0);
 
     return test;
 }
