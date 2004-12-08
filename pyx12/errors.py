77bb36276b0c7e58c7faacee435dc24be5514aff
John Holland <azoner@gmail.com>
2004-12-08 21:21:02 +0000

    removed unused errors

--- pyx12/errors.py
+++ pyx12/errors.py
@@ -19,30 +19,30 @@ class XML_Reader_Error(Exception):
 class x12Error(Exception):
     """Base class for X12N format errors."""
 
-class ISAError(x12Error):
-    """Errors in the ISA or IEA segements."""
+#class ISAError(x12Error):
+#    """Errors in the ISA or IEA segements."""
 
-class GSError(x12Error):
-    """Errors in the GS or GE segements."""
+#class GSError(x12Error):
+#    """Errors in the GS or GE segements."""
 
-class STError(x12Error):
-    """Errors in the ST or SE segements."""
+#class STError(x12Error):
+#    """Errors in the ST or SE segements."""
 
-class WEDIError(Exception):
-    """Base class for WEDI errors in this module."""
+#class WEDIError(Exception):
+#    """Base class for WEDI errors in this module."""
 
-class WEDI1Error(WEDIError): pass
-class WEDI2Error(WEDIError): pass
-class WEDI3Error(WEDIError): pass
-class WEDI4Error(WEDIError): pass
-class WEDI5Error(WEDIError): pass
-class WEDI6Error(WEDIError): pass
+#class WEDI1Error(WEDIError): pass
+#class WEDI2Error(WEDIError): pass
+#class WEDI3Error(WEDIError): pass
+#class WEDI4Error(WEDIError): pass
+#class WEDI5Error(WEDIError): pass
+#class WEDI6Error(WEDIError): pass
 
 class EngineError(Exception): 
     """Base class for translation engine errors."""
 
-class HL_Loop_Pop(EngineError):
-    """Pop a HL level"""
+#class HL_Loop_Pop(EngineError):
+#    """Pop a HL level"""
 
 class IterOutOfBounds(Exception):
     """Iterator is out of bounds"""
