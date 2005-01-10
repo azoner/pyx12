0fc60449bce3c9985ac31dc4859b412824e854bb
John Holland <azoner@gmail.com>
2005-01-10 06:50:05 +0000

    XML based config files

--- pyx12/params.py
+++ pyx12/params.py
@@ -15,40 +15,60 @@ Holds Run-time Parameters
 
 Order of precedence:
  1. set(param) - Command line parameters
- 2. '~/.pyx12rc' - User's directory
- 3. '/usr/local/etc/pyx12.conf' - Site default
- 4. self.params - Defaults
+ 2. Config file given on command line using -c
+ 3. '~/.pyx12rc' - User's directory
+ 4. '/usr/local/etc/pyx12.conf' - Site default
+ 5. self.params - Defaults
 """
 import os.path
-import ConfigParser
-import StringIO
+#import StringIO
+import libxml2
 
 class params:
     def __init__(self, config_file=None):
-        default = """
-[Main]
-map_path=/usr/local/share/pyx12/map
-pickle_path=%(map_path)s
-exclude_external_codes=
-[Validation]
-ignore_syntax=False
-charset=E
-ignore_codes=False
-ignore_ext_codes=False
-[Output]
-skip_html=False
-skip_997=False
-simple_dtd=http://www.kazoocmh.org/x12simple.dtd
-idtag_dtd=http://www.kazoocmh.org/x12idtag.dtd
-"""
-        self.cfg = ConfigParser.SafeConfigParser()
-        self.cfg.readfp(StringIO.StringIO(default))
+        self.params = {}
+        self.params['map_path'] = '/usr/local/share/pyx12/map'
+        self.params['pickle_path'] = '/usr/local/share/pyx12/map'
+        self.params['exclude_external_codes'] = None
+        self.params['ignore_syntax'] = False
+        self.params['charset'] = 'E'
+        self.params['ignore_codes'] = False
+        self.params['ignore_ext_codes'] = False
+        self.params['skip_html'] = False
+        self.params['skip_997'] = False
+        self.params['simple_dtd'] = 'http://www.kazoocmh.org/x12simple.dtd'
+        self.params['idtag_dtd'] = 'http://www.kazoocmh.org/x12idtag.dtd'
+        
+        self._get_config_file('/usr/local/etc/pyx12.conf')
+        self._get_config_file(os.path.expanduser('~/.pyx12rc'))
         if config_file:
-            self.cfg.read(['/usr/local/etc/pyx12.conf', \
-                os.path.expanduser('~/.pyx12rc'), config_file])
-        else:
-            self.cfg.read(['/usr/local/etc/pyx12.conf', \
-                os.path.expanduser('~/.pyx12rc')])
+            self._get_config_file(config_file)
+
+    def _get_config_file(self, filename):
+        if os.path.isfile(filename):
+            reader = libxml2.newTextReaderFilename(filename)
+            ret = self.reader.Read()
+            while ret == 1:
+                tmpNodeType = self.reader.NodeType()
+                if tmpNodeType == NodeType['element_start']:
+                    option = None
+                    value = None
+                    cur_name = self.reader.Name()
+                    if cur_name == 'transaction':
+                        base_name = 'transaction'
+                    elif cur_name == 'param':
+                        base_name = 'param'
+                        option = @name
+                    elif cur_name == 'type':
+                        base_name = 'type'
+                elif tmpNodeType == NodeType['element_end']:
+                    if option and value is not None:
+                        self.params[option] = value
+                elif tmpNodeType == NodeType['text']:
+                    if cur_name == 'id' and self.base_name == 'transaction':
+                        value = self.reader.Value()
+                        self.id = self.reader.Value()
+                ret = self.reader.Read()
 
     def get(self, option):
         """
@@ -56,16 +76,10 @@ idtag_dtd=http://www.kazoocmh.org/x12idtag.dtd
         @param option: Option name
         @type option: string
         """
-        for section in self.cfg.sections():
-            if self.cfg.has_option(section, option):
-                try:
-                    return self.cfg.getboolean(section, option)
-                except ValueError:
-                    try:
-                        return self.cfg.get(section, option)
-                    except ValueError:
-                        pass
-        return None
+        if option in self.params.keys():
+            return self.params[option]
+        else:
+            return None
 
     def set(self, option, value):
         """
@@ -75,19 +89,7 @@ idtag_dtd=http://www.kazoocmh.org/x12idtag.dtd
         @param value: Parameter value
         @type value: string
         """
-        for section in self.cfg.sections():
-            if self.cfg.has_option(section, option):
-                try:
-                    if value is False:
-                        self.cfg.set(section, option, 'False')
-                    elif value is True:
-                        self.cfg.set(section, option, 'True')
-                    else:
-                        self.cfg.set(section, option, value)
-                except:
-                    pass
+        self.params[option] = value
 
-    def __repr__(self):
-        fd_out = StringIO.StringIO() 
-        self.cfg.write(fd_out)
-        return fd_out.getvalue()
+#    def __repr__(self):
+#        pass
