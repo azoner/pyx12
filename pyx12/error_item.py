a5b111df104fac4e4a2f2bc052f74b33d19daf93
John Holland <azoner@gmail.com>
2005-08-10 07:03:22 +0000

    error item

--- pyx12/error_item.py
+++ pyx12/error_item.py
@@ -15,11 +15,18 @@
 
 import errors
 
+isa_errors = ('000' ,'001' ,'002' ,'003' ,'004' ,'005' ,'006' ,'007' ,'008' ,
+    '009' ,'010' ,'011' ,'012' ,'013' ,'014' ,'015' ,'016' ,
+    '017' ,'018' ,'019' ,'020' ,'021' ,'022' ,'023' ,'024' ,
+    '025' ,'026' ,'027' ,'028' ,'029' ,'030' ,'031')
+seg_errors = ('1', '2', '3', '4', '5', '6', '7', '8')
+ele_errors = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10')
+
 class ErrorItem:
     """
     Wrap an X12 validation error
     """
-    def __init__(self, err_type, err_cde, err_str, err_val=None, src_line=None):
+    def __init__(self, err_cde, err_str):
         """
         @param err_type: At what level did the error occur
         @type err_type: string
@@ -31,25 +38,8 @@ class ErrorItem:
         @param 
         @type 
         """
-        isa_errors = ('000' ,'001' ,'002' ,'003' ,'004' ,'005' ,'006' ,'007' ,'008' ,
-            '009' ,'010' ,'011' ,'012' ,'013' ,'014' ,'015' ,'016' ,
-            '017' ,'018' ,'019' ,'020' ,'021' ,'022' ,'023' ,'024' ,
-            '025' ,'026' ,'027' ,'028' ,'029' ,'030' ,'031')
-        seg_errors = ('1', '2', '3', '4', '5', '6', '7', '8')
-        ele_errors = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10')
-        if err_type == 'isa' and err_cde not in isa_errors:
-            raise error.EngineError, 'Invalid ISA level error code "%s"' % (err_cde) 
-        elif err_type in ('gs', 'st', 'seg') and and err_cde not in seg_errors:
-            raise error.EngineError, 'Invalid Segment level error code "%s"' % (err_cde) 
-        self.err_type = err_type
         self.err_cde = err_cde
         self.err_str = err_str
-        self.err_val = err_val
-        self.src_line = src_line
-
-
-    def getErrType(self):
-        return self.err_type
 
     def getErrCde(self):
         return self.err_cde
@@ -57,11 +47,44 @@ class ErrorItem:
     def getErrStr(self):
         return self.err_str
 
+
+class ISAError(ErrorItem):
+    def __init__(self, err_cde, err_str):
+        ErrorItem.__init__(self, err_cde, err_str)
+        if self.err_cde not in isa_errors:
+            raise errors.EngineError, 'Invalid ISA level error code "%s"' % \
+                (self.err_cde) 
+
+
+class SegError(ErrorItem):
+    def __init__(self, err_cde, err_str, err_val=None):
+        ErrorItem.__init__(self, err_cde, err_str)
+        self.err_val = err_val
+        if self.err_cde not in seg_errors:
+            raise errors.EngineError, 'Invalid segment level error code "%s"' % \
+                (self.err_cde) 
+
     def getErrVal(self):
         return self.err_val
 
-    def getSrcLine(self):
-        return self.src_line
 
+class EleError(ErrorItem):
+    def __init__(self, err_cde, err_str, ele_idx, subele_idx=None, 
+            err_val=None):
+        ErrorItem.__init__(self, err_cde, err_str)
+        self.err_val = err_val
+        self.ele_idx = ele_idx
+        self.subele_idx = subele_idx
+        if self.err_cde not in ele_errors:
+            raise errors.EngineError, 'Invalid element level error code "%s"' % \
+                (self.err_cde) 
 
+    def getErrVal(self):
+        return self.err_val
+
+    def getEleIdx(self):
+        return self.ele_idx
+
+    def getSubeleIdx(self):
+        return self.subele_idx
 
