4fea0b3fd35afecfc36f8ac8569c01d36b7d96c1
John Holland <azoner@gmail.com>
2006-02-21 16:59:49 +0000

    move unit test logic to sub-module

---
+++ pyx12/tests/__init__.py
@@ -0,0 +1,15 @@
+######################################################################
+# Copyright (c) 2001-2005 Kalamazoo Community Mental Health Services,
+#   John Holland <jholland@kazoocmh.org> <john@zoner.org>
+# All rights reserved.
+#
+# This software is licensed as described in the file LICENSE.txt, which
+# you should have received as part of this distribution.
+#
+######################################################################
+
+#    $Id$
+
+from pyx12.tests import map_if, params, syntax, x12xml_idtag
+from pyx12.tests import codes, segment, utils, path, x12file
+#from pyx12.tests import map_walker, map_index
---
+++ pyx12/tests/params.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env /usr/local/bin/python
+
+import unittest
+import sys
+
+import pyx12.params
+from pyx12.errors import *
+
+class Default(unittest.TestCase):
+    def setUp(self):
+        self.param = pyx12.params.params()
+
+    def test_valid1(self):
+        self.assertEqual(self.param.get('charset'), 'E')
+
+    def test_valid2(self):
+        self.assertEqual(self.param.get('skip_997'), False)
+
+
+class ClearParam(unittest.TestCase):
+    def setUp(self):
+        self.param = pyx12.params.params()
+
+    def test_valid1(self):
+        self.param.set('simple_dtd', '')
+        self.assertEqual(self.param.get('simple_dtd'), None)
+        self.param.set('simple_dtd', 'aaa')
+        self.assertEqual(self.param.get('simple_dtd'), 'aaa')
+
+    def test_valid2(self):
+        self.param.set('exclude_external_codes', '')
+        self.assertEqual(self.param.get('exclude_external_codes'), None)
+
+
+class SetParamOverride(unittest.TestCase):
+    def setUp(self):
+        self.param = pyx12.params.params()
+
+    def test_valid1(self):
+        self.param.set('ignore_syntax', False)
+        self.assertEqual(self.param.get('ignore_syntax'), False)
+        self.param.set('ignore_syntax', True)
+        self.assertEqual(self.param.get('ignore_syntax'), True)
+
+    def test_valid2(self):
+        self.param.set('exclude_external_codes', 'states,diagnosis')
+        self.assertEqual(self.param.get('exclude_external_codes'), 'states,diagnosis')
+
+
+class ReadConfigFile(unittest.TestCase):
+    def setUp(self):
+        self.param = pyx12.params.params('pyx12test.conf.xml')
+
+    def test_changed(self):
+        self.assertEqual(self.param.get('map_path'), '/opt1/local/share/pyx12/map')
+        self.assertEqual(self.param.get('exclude_external_codes'), 'taxonomy,states')
+        self.assertEqual(self.param.get('ignore_syntax'), True)
+        self.assertEqual(self.param.get('charset'), 'B')
+
+    def test_unchanged(self):
+        self.assertEqual(self.param.get('pickle_path'), '/usr/local/share/pyx12/map')
+        self.assertEqual(self.param.get('ignore_codes'), False)
+        self.assertEqual(self.param.get('skip_html'), False)
