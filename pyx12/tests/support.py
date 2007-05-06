dcd912cc3a44dc6f84609402204ec14fcde2f624
John Holland <azoner@gmail.com>
2007-05-06 06:16:44 +0000

    cleanup path lookup in tests

--- pyx12/tests/support.py
+++ pyx12/tests/support.py
@@ -11,12 +11,6 @@ def getMapPath():
     First, try relative path
     Then look in standard installation location
     """
-    base_dir = dirname(dirname(abspath(sys.argv[0])))
-    map_path = join(base_dir, 'map')
-    if isdir(map_path):
-        return map_path
     params = pyx12.params.params()
     map_path = params.get('map_path')
-    if isdir(map_path):
-        return map_path
-    return None
+    return map_path
