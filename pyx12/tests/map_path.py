9577f71396e62b9c702cd150cf1350df16957c63
John Holland <azoner@gmail.com>
2008-12-08 17:27:09 +0000

    test map_path

--- pyx12/tests/map_path.py
+++ pyx12/tests/map_path.py
@@ -11,8 +11,9 @@ class MapPath(unittest.TestCase):
 
     def test_get_map_path(self):
         map_path = getMapPath()
-        param = pyx12.params.params('pyx12.conf.xml')
         self.assertNotEqual(map_path, '/usr/local/share/pyx12/map')
-        self.assertNotEqual(param.get('map_path'), '/usr/local/share/pyx12/map')
+        param = pyx12.params.params('pyx12.conf.xml')
+        param.set('map_path', map_path)
+        self.assertEqual(param.get('map_path'), map_path)
         file837 = join(map_path, '837.4010.X098.A1.xml')
         self.failUnless(isfile(file837))
