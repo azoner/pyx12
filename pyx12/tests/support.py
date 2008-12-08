66fcc3e06b281f9dbed0d7a1c3e85828d13e4d07
John Holland <azoner@gmail.com>
2008-12-08 17:26:31 +0000

    support map_path

--- pyx12/tests/support.py
+++ pyx12/tests/support.py
@@ -1,6 +1,4 @@
-#! /usr/bin/env /usr/local/bin/python
-
-#    $Id: map_walker.py 935 2007-03-21 13:49:34Z johnholland $
+#    $Id$
 
 import sys
 from os.path import dirname, abspath, join, isdir, isfile
@@ -11,6 +9,11 @@ def getMapPath():
     First, try relative path
     Then look in standard installation location
     """
-    params = pyx12.params.params()
-    map_path = params.get('map_path')
-    return map_path
+    base_dir = dirname(dirname(abspath(sys.argv[0])))
+    map_path = join(base_dir, 'map')
+    if isdir(map_path):
+        return map_path
+    else:
+        params = pyx12.params.params()
+        map_path = params.get('map_path')
+        return map_path
