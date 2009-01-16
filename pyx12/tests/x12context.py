9dd56d683bccfeaa0ad392e6a5d27e9b7fbf650c
John Holland <azoner@gmail.com>
2009-01-16 21:54:43 +0000

    context node exists bug

--- pyx12/tests/x12context.py
+++ pyx12/tests/x12context.py
@@ -194,6 +194,26 @@ class TreeAddSegmentString(unittest.TestCase):
         self.failUnlessRaises(pyx12.errors.X12PathError, self.loop2300.add_segment, 'ZZZ*00~')
 
 
+class SegmentExists(unittest.TestCase):
+
+    def setUp(self):
+        fd = open('files/simple_837p.txt')
+        param = pyx12.params.params('pyx12.conf.xml')
+        errh = pyx12.error_handler.errh_null()
+        self.src = pyx12.x12context.X12ContextReader(param, errh, fd, xslt_files = [])
+        for datatree in self.src.iter_segments('2300'):
+            if datatree.id == '2300':
+                self.loop2300 = datatree
+                break
+
+    def test_qual_segment(self):
+        self.failUnless(self.loop2300.exists('2310B'))
+        self.failUnless(self.loop2300.exists('2310B/NM1[82]'))
+        for loop2310b in self.loop2300.select('2310B'):
+            self.failUnless(loop2310b.exists('NM1'))
+            self.failUnless(loop2310b.exists('NM1[82]'))
+
+
 class TreeAddLoop(unittest.TestCase):
 
     def setUp(self):
@@ -213,6 +233,7 @@ class TreeAddLoop(unittest.TestCase):
         self.failUnless(self.loop2300.exists('2310B'))
         for loop2310b in self.loop2300.select('2310B'):
             self.failUnless(loop2310b.exists('NM1'))
+            self.failUnless(loop2310b.exists('NM1[82]'))
 
     def test_add_new_string_seg(self):
         old_ct = self.loop2300.count('2400')
