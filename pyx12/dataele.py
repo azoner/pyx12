9e4fef10d00117472ce6d891eba5b26fd3f9076b
John Holland <azoner@gmail.com>
2011-10-29 07:55:27 +0000

    move dataele to ElementTree XML

--- pyx12/dataele.py
+++ pyx12/dataele.py
@@ -15,11 +15,7 @@ Interface to normalized Data Elements
 """
 
 import os, os.path
-#import sys
-#import re
-import cPickle
-import libxml2
-from stat import ST_MTIME
+from xml.etree.ElementTree import ElementTree
 
 # Intrapackage imports
 from pyx12.errors import EngineError, XML_Reader_Error
@@ -48,66 +44,15 @@ class DataElements(object):
         
         self.dataele = {} 
         code_file = os.path.join(base_path, 'dataele.xml')
-        pickle_file = '%s.%s' % (os.path.splitext(code_file)[0], 'pkl')
-        
-        ele_num = None
-        data_type = None
-        min_len = None
-        max_len = None
-        name = None
-        # init the map of dataele from the pickled file dataele.pkl
-        try:
-            if os.stat(code_file)[ST_MTIME] < os.stat(pickle_file)[ST_MTIME]:
-                self.dataele = cPickle.load(open(pickle_file, 'b'))
-            else: 
-                raise DataElementsError, "reload data elements"
-        except:
-            try:
-                reader = libxml2.newTextReaderFilename(code_file)
-            except:
-                raise EngineError, 'Data Element file not found: %s' % (code_file)
-            try:
-                ret = reader.Read()
-                if ret == -1:
-                    raise XML_Reader_Error, 'Read Error'
-                elif ret == 0:
-                    raise XML_Reader_Error, 'End of Map File'
-                while ret == 1:
-                    if reader.NodeType() == NodeType['element_start']:
-                        cur_name = reader.Name()
-                        if cur_name == 'data_ele':
-                            while reader.MoveToNextAttribute():
-                                if reader.Name() == 'ele_num':
-                                    ele_num = reader.Value()
-                                elif reader.Name() == 'data_type':
-                                   data_type = reader.Value()
-                                elif reader.Name() == 'min_len':
-                                   min_len = int(reader.Value())
-                                elif reader.Name() == 'max_len':
-                                   max_len = int(reader.Value())
-                                elif reader.Name() == 'name':
-                                   name = reader.Value()
-                            if ele_num is None or data_type is None or \
-                                    min_len is None or max_len is None or \
-                                    name is None:
-                                raise EngineError, 'Invalid Data Element %s' \
-                                    % (ele_num)
-                            self.dataele[ele_num] = (data_type, min_len, max_len) #, name)
-                    elif reader.NodeType() == NodeType['element_end']:
-                        if cur_name == 'data_ele':
-                            ele_num = None
-                            data_type = None
-                            min_len = None
-                            max_len = None
-                            name = None
-                    ret = reader.Read()
-                    if ret == -1:
-                        raise XML_Reader_Error, 'Read Error'
-                    elif ret == 0:
-                        raise XML_Reader_Error, 'End of Map File'
-            except XML_Reader_Error:
-                pass
-
+        tree = ElementTree()
+        t = tree.parse(code_file)
+        for e in t.iter('data_ele'):
+            ele_num = e.get('ele_num')
+            data_type = e.get('data_type')
+            min_len = int(e.get('min_len'))
+            max_len = int(e.get('max_len'))
+            name = e.get('name')
+            self.dataele[ele_num] = (data_type, min_len, max_len, name)
 
     def get_by_elem_num(self, ele_num):
         """
@@ -122,10 +67,6 @@ class DataElements(object):
         if not self.dataele.has_key(ele_num):
             raise EngineError, 'Data Element "%s" is not defined' \
                 % (ele_num)
-        #data_type = self.dataele[ele_num][0]
-        #min_len = self.dataele[ele_num][1]
-        #max_len = self.dataele[ele_num][2]
-        # name = self.dataele[ele_num][3]
         return self.dataele[ele_num]
 
     def __repr__(self):
