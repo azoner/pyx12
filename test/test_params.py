0f83be140b6f8e5162558149cf563ecd414531fa
John Holland <azoner@gmail.com>
2011-10-29 23:46:17 +0000

    remove psyco

--- test/test_params.py
+++ test/test_params.py
@@ -7,11 +7,6 @@ from pyx12.tests.params import *
 from pyx12.errors import *
 from helper import get_testcases, print_testcases, get_suite
 
-try:
-    import psyco
-    psyco.full()
-except ImportError:
-    pass
 ns = pyx12.tests.params
 if len(sys.argv) > 1 and sys.argv[1] == '-h':
     print_testcases(ns)
