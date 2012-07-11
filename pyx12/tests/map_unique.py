17f1ee03fb7c84b3c9bbae33a559d2e7e78075ea
John Holland <azoner@gmail.com>
2012-07-11 14:19:00 -0400

    disable 4010/5010 loop path tests - will never match

--- pyx12/tests/map_unique.py
+++ pyx12/tests/map_unique.py
@@ -37,35 +37,35 @@ class UniqueNodePath(unittest.TestCase):
             self.assertTrue(p not in paths, 'Duplicate path %s' % (p))
             paths.add(p)
 
-    def test_835(self):
+    def xtest_835(self):
         self.maxDiff = None
         (f1, f2) = ('835.4010.X091.A1.xml', '835.5010.X221.A1.xml')
         old = self._get_paths(f1)
         new = self._get_paths(f2)
         self.assertSequenceEqual(old, new)
 
-    def test_834(self):
+    def xtest_834(self):
         self.maxDiff = None
         (f1, f2) = ('834.4010.X095.A1.xml', '834.5010.X220.A1.xml')
         old = self._get_paths(f1)
         new = self._get_paths(f2)
         self.assertSequenceEqual(old, new)
 
-    def test_820(self):
+    def xtest_820(self):
         self.maxDiff = None
         (f1, f2) = ('820.4010.X061.A1.xml', '820.5010.X218.xml')
         old = self._get_paths(f1)
         new = self._get_paths(f2)
         self.assertSequenceEqual(old, new)
 
-    def test_837p(self):
+    def xtest_837p(self):
         self.maxDiff = None
         (f1, f2) = ('837.4010.X098.A1.xml', '837.5010.X222.A1.xml')
         old = self._get_paths(f1)
         new = self._get_paths(f2)
         self.assertSequenceEqual(old, new)
 
-    def test_837i(self):
+    def xtest_837i(self):
         self.maxDiff = None
         (f1, f2) = ('837.4010.X096.A1.xml', '837Q3.I.5010.X223.A1.xml')
         old = self._get_paths(f1)
