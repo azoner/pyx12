83302ee846c62b32ec599ec0d702259abf9bfb41
John Holland <azoner@gmail.com>
2013-08-08 16:13:06 -0400

    prep for v2.0.1

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 from setuptools import setup
 
 
-__version__ = ""
-execfile('pyx12/version.py')
 long_description = """Pyx12 is a HIPAA X12 document validator and converter. It parses an ANSI X12N data file and
 validates it against a representation of the Implementation Guidelines for a HIPAA transaction. By default, it
 creates a 997 response for 4010 and a 999 response for 5010. It can create an html representation of the X12
@@ -10,14 +8,14 @@ document or can translate to and from an XML representation of the data file."""
 
 setup(
     name="pyx12",
-    version=__version__,
+    version="2.0.1",
     long_description=long_description,
     license='BSD',
     description="HIPAA X12 validator, parser and converter",
     keywords='x12 hipaa healthcare edi',
     author="John Holland",
     author_email="jholland@kazoocmh.org",
-    url="http://github.com/azoner/pyx12#pyx12",
+    url="http://github.com/azoner/pyx12",
     platforms='All',
     packages=['pyx12', 'pyx12.scripts'],
     package_data={
