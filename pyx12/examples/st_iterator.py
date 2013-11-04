c10c4ce99c979d726d014dc10d120849443f5c06
John Holland <azoner@gmail.com>
2013-11-04 17:22:13 -0500

    cleanup st loop iterator example

--- pyx12/examples/st_iterator.py
+++ pyx12/examples/st_iterator.py
@@ -1,54 +1,87 @@
 #! /usr/bin/env python
 import sys
-import logging
 from itertools import groupby
+import tempfile
+import random
 
 import pyx12
 import pyx12.x12context
-import pyx12.params
-
-__author__ = 'John Holland'
-__version__ = '1.0'
-__date__ = '2011-01-22'
-
-"""
-Populate tables with 999 data
-"""
-
-VERBOSE = 0
-
-logger = logging.getLogger()
-sub_idx = 0
 
 
-def st_generator():
+def x12_split_on_st(source_filename, isa_id=11, gs_id=21):
+    src = pyx12.x12file.X12Reader(source_filename)
+    idx = -1
+    for k, g in groupby(get_headers_stream(src), lambda x: x[0]):
+        idx += 1
+        #import ipdb; ipdb.set_trace()
+        st_id = int(k['st_seg'].get_value('ST02'))
+        fd_temp = tempfile.TemporaryFile()
+        wr = pyx12.x12file.X12Writer(fd_temp)
+        wr.Write(update_isa_id(k['isa_seg'], isa_id + idx))
+        wr.Write(update_gs_id(k['gs_seg'], gs_id + idx))
+        for seg in g:
+            wr.Write(seg[1])
+        wr.Close()  # Auto close ST, GS and ISA loops
+        yield (isa_id + idx, gs_id + idx, st_id, fd_temp)
+
+
+def save_many(src_filename, targetformat=None):
+    base_isa_id = random.randint(1000, 999999999)
+    base_gs_id = random.randint(100, 999999999)
+    for (isa_id, gs_id, st_id, fd_temp) in x12_split_on_st(src_filename, base_isa_id, base_gs_id):
+        if targetformat is not None:
+            newname = targetformat.format(isa_id=isa_id, gs_id=gs_id, st_id=st_id)
+        else:
+            newname = "newfile_{isa_id}.txt".format(isa_id=isa_id)
+        fd_out = open(newname, 'w')
+        fd_temp.seek(0)
+        fd_out.write(fd_temp.read())
+        fd_out.close()
+        print newname, isa_id, gs_id, st_id
+
+
+def update_isa_id(seg_data, isa_id):
+    seg_data.set('ISA13', "{0:0>9}".format(int(isa_id)))
+    return seg_data
+
+
+def update_gs_id(seg_data, gs_id):
+    seg_data.set('GS06', "{0}".format(int(gs_id)))
+    return seg_data
+
+
+def get_headers_stream(segments):
     """
+    passed a segment enumerable
+    yields (isa_segment, gs_segment, st_segment, current_segment)
     """
-    testfile = 'multiple_st_loops.txt'
-    #wr = edifile.WriteFile(conn)
-    fd_in = open(testfile)
     isa_seg = None
     gs_seg = None
-    isa_id = 11
-    gs_id = 21
-    #for k, g in groupby(iterate_2000(fd_in), lambda x: x['st_id']):
-    for k, g in groupby(iterate_2000(fd_in), lambda x: x[0]):
-        #yield (k, g)
-        print '-----------------------------------------------------------'
-        print k
-        for d in g:
-            yield d
-        print '-----------------------------------------------------------'
-    #for d in iterate_2000(fd_in):
-    #    yield d
+    st_seg = None
+    for seg_data in segments:
+        seg_id = seg_data.get_seg_id()
+        if seg_id == 'ISA':
+            isa_seg = seg_data
+        elif seg_id == 'GS':
+            gs_seg = seg_data
+        elif seg_id in ('IEA', 'GE'):
+            pass
+        else:
+            if seg_id == 'ST':
+                st_seg = seg_data
+            k = {
+                'isa_seg': isa_seg,
+                'gs_seg': gs_seg,
+                'st_seg': st_seg,
+            }
+            v = seg_data
+            yield (k, v)
 
 
 def iterate_2000(fd_in):
     param = pyx12.params.params()
     errh = pyx12.error_handler.errh_null()
     src = pyx12.x12context.X12ContextReader(param, errh, fd_in)
-    #isa_id = None
-    #gs_id = None
     isa_seg = None
     gs_seg = None
     st_id = None
@@ -77,8 +110,9 @@ def iterate_2000(fd_in):
 
 
 def main():
-    for s in st_generator():
-        print '\t' + s[1].format()
+    testfile = 'multiple_st_loops.txt'
+    targetformat = None
+    save_many(testfile, targetformat)
     return True
 
 if __name__ == '__main__':
