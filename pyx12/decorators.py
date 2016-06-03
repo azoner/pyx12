d592e2a3e076fc7d73ea9ad0ed486d3780656966
John Holland <azoner@gmail.com>
2016-06-03 11:23:02 -0400

    add standard memoize decorators

--- pyx12/decorators.py
+++ pyx12/decorators.py
@@ -1,3 +1,8 @@
+import functools
+import collections
+
+# See https://wiki.python.org/moin/PythonDecoratorLibrary
+
 def dump_args(func):
     "This decorator dumps out the arguments passed to a function before calling it"
     argnames = func.func_code.co_varnames[:func.func_code.co_argcount]
@@ -9,3 +14,45 @@ def dump_args(func):
         return func(*args, **kwargs)
 
     return echo_func
+
+
+def memoize(obj):
+    cache = obj.cache = {}
+
+    @functools.wraps(obj)
+    def memoizer(*args, **kwargs):
+        if kwargs:  # frozenset is used to ensure hashability
+            key = args, frozenset(kwargs.iteritems())
+        else:
+            key = args
+        if key not in cache:
+            cache[key] = obj(*args, **kwargs)
+        return cache[key]
+    return memoizer
+
+
+class memoized(object):
+    '''Decorator. Caches a function's return value each time it is called.
+    If called later with the same arguments, the cached value is returned
+    (not reevaluated).
+    '''
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+    def __call__(self, *args):
+        if not isinstance(args, collections.Hashable):
+            # uncacheable. a list, for instance.
+            # better to not cache than blow up.
+            return self.func(*args)
+        if args in self.cache:
+            return self.cache[args]
+        else:
+            value = self.func(*args)
+            self.cache[args] = value
+            return value
+    def __repr__(self):
+        '''Return the function's docstring.'''
+        return self.func.__doc__
+    def __get__(self, obj, objtype):
+        '''Support instance methods.'''
+        return functools.partial(self.__call__, obj)
